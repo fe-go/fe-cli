0f72421504af34136b5eb41e8024b74504923b9f
李卓 <lizhuo@lizhuodeMacBook-Pro.local>
2019-02-17 22:30:34 +0800

    update:

--- example/index.js
+++ example/index.js
@@ -1,12 +1,24 @@
-const indexCreater = require("../index")
+const indexCreater = require("../index");
 
-indexCreater({
-  root: "components",
-  match: "**/!(*.*)", // 此处参数为glob类型
-  separator: /(-|_)/g,
-  exportPattern: `export { default as [name] , I[name]Props } from '[path]'`,
-  suffix: ".ts",
-  callback(template, items) {
-   return template + '\n// test'
+indexCreater([
+  {
+    root: "components",
+    match: "**/!(*.*)", // 此处参数为glob类型
+    separator: /(-|_)/g,
+    exportPattern: `export { default as [name] , I[name]Props } from '[path]'`,
+    suffix: ".ts",
+    callback(template, items) {
+      return template + "\n// test";
+    }
+  },
+  {
+    root: "components",
+    match: "**/!(*.*)", // 此处参数为glob类型
+    separator: /(-|_)/g,
+    exportPattern: `export { default as [name] } from '[path]'`,
+    suffix: ".jsx",
+    callback(template, items) {
+      return template + "\n// test ll";
+    }
   }
-})
+]);
