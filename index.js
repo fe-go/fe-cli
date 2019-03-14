f084ac957981bac3258fc103bf627299b8aa7b3d
advence-liz <1012515183@qq.com>
2019-03-14 10:43:16 +0800

    upd:优化一下IDE智能提示的内容，去掉垃圾话

--- index.js
+++ index.js
@@ -133,8 +133,13 @@ function getPascal(x, separator) {
   return str.replace(separator, "");
 }
 /**
- * 整理参数传入 createIndex 的参数默认支持两种形式，倒不是因为接口支持的格式越多越牛B只是因为之前没考虑批处理为了兼容所以支持两种格式
- * @param {Object|Array} config
+ * @param {Object|Array} config 
+ * @prop {String} match 子模块的匹配规则 glob pattern
+ * @prop {Regex} separator 子模块目录(文件)命名分割符默认为 /(-|_)/g
+ * @prop {String} exportPattern 子模块导出模板
+ * @prop {String} suffix 生成index文件的后缀默认为 '.js'
+ * @prop {String|Array} ignore glob pattern or glob pattern array
+ * @prop {(template,items)=>String} callback 自定义回调函数,template 为引进生成的文件模板，items 匹配的文件信息，返回值为新的模板
  */
 module.exports = config => {
   let configs = [config];
