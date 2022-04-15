9de7c7287af7499d1508659deb71ade347402c35
advnece-liz <advence_liz@163.com>
2022-04-15 18:53:04 +0800

    feat: 优化切片逻辑,限制切片时间

--- packages/fe-cli/cli.js
+++ packages/fe-cli/cli.js
@@ -9,86 +9,88 @@ require('./lib/checkNodeVersion')
 const notifier = updateNotifier({ pkg })
 
 if (notifier.update) {
-  console.log(`Update available: ${notifier.update.latest}`)
+    console.log(`Update available: ${notifier.update.latest}`)
 }
 
 program
-  .version(pkg.version, '-v, --version', 'version')
-  .option('--code', '使用 vscode 展示 diff')
-  .option('-S, --small', 'small qrcode size')
+    .version(pkg.version, '-v, --version', 'version')
+    .option('--code', '使用 vscode 展示 diff')
+    .option('-S, --small', 'small qrcode size')
 // .option('-p, --packages [value]', '新生成monorepo组件')
 // .option('--type <string>', '编译类型 dev,build,prepub,publish,test')
 
 program
-  .command('hls <src> <outdir>')
-  .description('视频切片')
-  .action(async (src, outdir) => {
-    console.log(src, outdir)
-    require('./script/hls')(src, outdir)
-  })
+    .command('hls <src> <outdir> [time]')
+    .description('视频切片')
+    .action(async (src, outdir, time = 2) => {
+        console.log(src, outdir)
+        require('./script/hls')(src, outdir, time)
+    })
 
 program
-  .command('diff <file1> <file2>')
-  .description('展示文件 diff')
-  .option('--code, --diff_mode <mode>', '通过 vscode 展示 DIFF', true)
-  .action(async (one, other) => {
-    require('./script/diff')(one, other, program.opts())
-  })
+    .command('diff <file1> <file2>')
+    .description('展示文件 diff')
+    .option('--code, --diff_mode <mode>', '通过 vscode 展示 DIFF', true)
+    .action(async (one, other) => {
+        require('./script/diff')(one, other, program.opts())
+    })
 
 program
-  .command('qrcode <url>')
-  .alias('qr')
-  .description('generate qrcode')
-  .action((url) => {
-    require('./script/qrcode')(url, program.opts())
-  })
+    .command('qrcode <url>')
+    .alias('qr')
+    .description('generate qrcode')
+    .action((url) => {
+        require('./script/qrcode')(url, program.opts())
+    })
 
 // get local public IP address
 program
-  .command('IP')
-  .alias('ip')
-  .description('get local public IP address')
-  .action(() => {
-    console.log(require('./script/ipAddress')())
-  })
+    .command('IP')
+    .alias('ip')
+    .description('get local public IP address')
+    .action(() => {
+        console.log(require('./script/ipAddress')())
+    })
 
 program
-  .command('iconfont <src> [dest]')
-  .description('去掉iconfont中冗余的引用资源,将需要引用的 .ttf 资源自动转化为 base64,简化 iconfont 引入方式。')
-  .action((src, dest = '.') => {
-    require('./script/iconfont')(src, dest, program.opts())
-  })
+    .command('iconfont <src> [dest]')
+    .description(
+        '去掉iconfont中冗余的引用资源,将需要引用的 .ttf 资源自动转化为 base64,简化 iconfont 引入方式。'
+    )
+    .action((src, dest = '.') => {
+        require('./script/iconfont')(src, dest, program.opts())
+    })
 
 program
-  .command('tinypng <src> [dest]')
-  .alias('ty')
-  .description('压缩指定目录下的 .png .jpg .jpeg 文件')
-  .action((src, dest = '.') => {
-    require('./script/tinypng')(src, dest, program.opts())
-  })
+    .command('tinypng <src> [dest]')
+    .alias('ty')
+    .description('压缩指定目录下的 .png .jpg .jpeg 文件')
+    .action((src, dest = '.') => {
+        require('./script/tinypng')(src, dest, program.opts())
+    })
 
 program.on('--help', function () {
-  console.log('')
-  console.log('Examples:')
-  console.log('  $ fe diff file1 file2')
-  console.log('  $ fe diff file1 file2 --code')
-  console.log('  $ fe hls xxx.mp4 outdir')
-  console.log('  $ fe qr URL')
-  console.log('  $ fe qr URL -S/--small')
-  console.log('  $ fe ty images outdir')
-  console.log('  $ fe tinypng images outdir')
-  console.log('  $ fe tinypng images')
-  console.log('  $ fe tinypng xx.png outdir')
+    console.log('')
+    console.log('Examples:')
+    console.log('  $ fe diff file1 file2')
+    console.log('  $ fe diff file1 file2 --code')
+    console.log('  $ fe hls xxx.mp4 outdir')
+    console.log('  $ fe qr URL')
+    console.log('  $ fe qr URL -S/--small')
+    console.log('  $ fe ty images outdir')
+    console.log('  $ fe tinypng images outdir')
+    console.log('  $ fe tinypng images')
+    console.log('  $ fe tinypng xx.png outdir')
 })
 
 try {
-  program.parse(process.argv)
+    program.parse(process.argv)
 } catch (err) {
-  // 逻辑执行错误此处也会抛出
-  console.log(err)
-  // program.outputHelp()
+    // 逻辑执行错误此处也会抛出
+    console.log(err)
+    // program.outputHelp()
 }
 
 if (!process.argv.slice(2).length) {
-  program.outputHelp()
+    program.outputHelp()
 }
--- packages/fe-cli/script/hls.js
+++ packages/fe-cli/script/hls.js
@@ -6,7 +6,7 @@ const { blue, red, green } = require('../lib/color')
 const Progressbar = require('../lib/progressbar')
 const ora = require('ora')
 
-module.exports = (src, outdir) => {
+module.exports = async (src, dist, time) => {
   // https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/blob/master/examples/livertmp2hls.js
   // make sure you set the correct path to your video file
   try {
@@ -19,16 +19,18 @@ module.exports = (src, outdir) => {
     // console.log(error.toString())
     process.exit(0)
   }
-  if (escape(src).indexOf('%u') > 0) {
-    console.log(red(`请确保文件名 ${src} 不包含中文否则上传会失败`))
-    process.exit(0)
-  }
+
+  fs.ensureDirSync(path.resolve(dist))
   const sourcePath = path.resolve(src)
-  const outputPath = path.resolve(outdir, `${path.parse(src).name}.m3u8`)
-  fs.ensureDirSync(path.resolve(outdir))
-  const spinner = ora('fe hls').start()
+  // 当文件名为中文是 s3 上传会失败，创建临时目录转移视频
+  const tempPath = path.resolve(dist, 'hls' + path.parse(src).ext)
+  await fs.renameSync(sourcePath, tempPath)
+  const outputPath = path.resolve(dist, `${path.parse(tempPath).name}.m3u8`)
+
+  const spinner = ora('progress').start()
   const pb = new Progressbar()
-  ffmpeg(sourcePath, { timeout: 432000 })
+  // ffmpeg -i output.mp4 -c:v libx264 -c:a aac -strict -2 -f hls -hls_list_size 0 -hls_time 5 output1.m3u8
+  ffmpeg(tempPath, { timeout: 432000 })
     // set video bitrate
     .videoBitrate(1024)
     // set h264 preset
@@ -38,23 +40,34 @@ module.exports = (src, outdir) => {
     // set audio bitrate
     // .audioBitrate('128k')
     // set audio codec
-    // .audioCodec('libfaac')
+    .audioCodec('aac')
     // set number of audio channels
     .audioChannels(2)
+    // 强制一秒一个关键帧
+    .addOption('-force_key_frames', 'expr:gte(t,n_forced*1)')
     // set hls segments time
-    .addOption('-hls_time', 10)
+
+    .addOption('-hls_time', Number(time))
     // include all the segments in the list
     .addOption('-hls_list_size', 0)
     .on('progress', function (progress) {
       spinner.color = 'yellow'
-      spinner.text = pb.render(progress.percent.toFixed(2))
+      const percent = progress.percent
+      if (!percent) return
+      spinner.text = pb.render(percent)
     })
     // setup event handlers
-    .on('end', function () {
+    .on('end', async function () {
       spinner.succeed(green(`${src} file has been converted succesfully`))
-      process.exit(1)
+      // ora('uploding').start()
+      // 恢复视频文件
+      await fs.renameSync(tempPath, sourcePath)
+      process.exit(0)
+      // spinner.succeed(green('upload done'))
+    
     })
-    .on('error', function (err) {
+    .on('error', async function (err) {
+      await fs.renameSync(tempPath, sourcePath)
       spinner.fail(red('an error happened: ') + err.message)
     })
     // save to file
